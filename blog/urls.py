c5239affd52aa71d7b5b71365723101cefb4c6ad
charith kabamba <ledouxkabamba135@@gmail.com>
2026-01-26 03:36:03 +0100

    nouveau

--- blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import PostListView, PostDetailView
-
-app_name = 'blog'
-
-urlpatterns = [
-    path('', PostListView.as_view(), name='list'),
-    path('<slug:slug>/', PostDetailView.as_view(), name='detail'),
-]
--- management/urls.py
+++ management/urls.py
@@ -1,16 +1,7 @@
 from django.urls import path
-from .views import *
+from .views import home
 
 urlpatterns = [
     path('', home, name='home'),
-    path('contact/', contact, name='contact'),
-    path('about/', about, name='about'),
-    path('blog/', blog, name='blog'),
-    path('service/', service, name='service'),
-    path('service/<str:id>/', service_details, name='service_details'),
-    path('project/', project, name='project'),
-    path('team/', team, name='team'),
-    path('testimonial/', testimonial, name='testimonial'),
-    path('404/', error_404_view, name='404'),
-    path('gallery/', gallery, name='gallery'),
+
 ]
